5bdfd53459e6de3b551a66ed9b359d63a07a99ee
Ben Parish <parishben13@gmail.com>
2020-05-07 23:34:50 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
+array=[1,2,3]
 def oxford_comma(array)
-
+if array.size ==1 
+  array.join
+  elsif array.size == 2 
+  array.join(" and ")
+  elsif array.size == 3
+  secondArr= array[array.length-1]
+  firstArr= array[0..1].join(",")
+  
+  firstArr << " and " << secondArr
+else "garbage"
 end
+end
+oxford_comma(array)
