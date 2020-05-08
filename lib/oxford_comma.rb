ec1161faf8e9448c271974b643f82661900abd7e
Ben Parish <parishben13@gmail.com>
2020-05-08 00:14:31 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,24 @@
-def oxford_comma(array)
 
+def oxford_comma(array)
+  if array.size ==1 
+    array.join
+  elsif array.size == 2 
+    array.join(" and ")
+  elsif array.size == 3
+    secondArr= []
+    secondArr << "and " << array[array.length-1]
+    secondArr.join(" ")
+    firstArr= array[0..1]
+    firstArr.join(",")
+    firstArr << secondArr.join
+    firstArr.join(", ")
+  elsif array.size > 3
+    bestArr= []
+    secondbestArr= []
+    bestArr << array[0..array.length-2]
+    bestArr.join(", ")
+    secondbestArr << "and " << array[array.length-1]
+    bestArr << secondbestArr.join
+    bestArr.join(", ")
+  end
 end
